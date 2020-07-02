a0977839a2f9edafe9b7cf596b0b98e0d0a72cb3
rajramasawmy <raj_ramasawmy@hotmail.com>
2020-07-01 22:35:16 -0400

    superhi example, new page

---
+++ src/blocks.js
@@ -0,0 +1,28 @@
+let xs = []
+for (var i = 0; i <= 500; i++) {
+  xs.push(i)
+}
+
+let t = 0
+
+function animate() {
+  
+  let points = xs.map(x => {
+    
+    let y = 40 + 20 * Math.sin((x + t) / 10)
+    
+    return [x, y]
+  })
+  
+  let path = "M" + points.map(p => {
+    return p[0] + "," + p[1]
+  }).join(" L")
+  
+  document.querySelector("path").setAttribute("d", path)
+  
+  t += 0.5
+  
+  requestAnimationFrame(animate)
+}
+
+animate()
